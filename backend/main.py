dc2aa4a595e171dcd557ce4a21c3c51843ce561e
PooriaT <pooria_taghdiri@hotmail.com>
2023-11-18 00:03:55 -0800

    restructuring main.py to mange concurrency - NEEDS FURTHER CHANGES

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,7 @@ from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+
 app = FastAPI()
 
 # Allow all origins in development. Adjust as needed for production.
@@ -21,6 +22,21 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Singeltont Pattern
+class GptClientWrapper:
+    def __init__(self):
+        self.gpt_client = GptApi()
+
+    def get_instance(self):
+        return self.gpt_client
+
+gpt_client_wrapper = GptClientWrapper()
+
+def get_gpt_client():
+    return gpt_client_wrapper.get_instance()
+
+
+
 def reading_data():
     file_path = './static/resume/resume.docx'
     file_extension = os.path.splitext(file_path)[1].lower()
@@ -30,16 +46,14 @@ def reading_data():
     elif file_extension == '.docx':
         data = json.loads(read_docx(file_path))
     else:
-        raise('File type not supported')
+        raise HTTPException(status_code=400, detail='File type not supported')
     return data
 
-
-@app.get("/upload_resume")
-async def upload_resume():
+def process_upload():
     print("UPLOAD")
     data = reading_data()
-    gptClinet = GptApi()
-    extracted_resume_data = gptClinet.extract_info_from_resume(data['text'])
+    gptClient = get_gpt_client()
+    extracted_resume_data = gptClient.extract_info_from_resume(data['text'])
 
     print("EXTRACTING is finished")
     
@@ -57,12 +71,9 @@ async def upload_resume():
     except Exception as e:
         print(f'Error: {e}')
 
-    return {'text': 'successfully uploaded'}
-
-@app.get("/compare_resume")
-async def compare_resume():
-    print("COMPARE")  
-    gptClinet = GptApi()
+async def process_compare():
+    print("COMPARE")
+    gptClient = get_gpt_client()
 
     with open('./static/resume/extracted_resume.json') as file:
         extracted_resume_data = file.read()
@@ -70,38 +81,51 @@ async def compare_resume():
     with open('./static/resume/job_description.txt') as file:
         job_description_data = file.read()
 
-    compared_data_stream = gptClinet.compare_resume_with_job_description(
+    return gptClient.compare_resume_with_job_description(
         extracted_resume_data, 
         job_description_data
         )
+
+async def process_revise():
+    print("REVISE")
+    gptClient = get_gpt_client()
+
+    with open('./static/resume/extracted_resume.json') as file:
+        extracted_resume_data = file.read()
+
+    with open('./static/resume/job_description.txt') as file:
+        job_description_data = file.read()
+
+    return gptClient.align_resume_info_with_job_description(
+        extracted_resume_data, 
+        job_description_data
+        )
+
+
+
+@app.get("/upload_resume")
+async def upload_resume():
+    await process_upload()
+    return {'text': 'successfully uploaded'}
+
+
+@app.get("/compare_resume")
+async def compare_resume():
+    compared_data_stream = await process_compare()
 
     # return StreamingResponse(compared_data_stream,
     #                            media_type='text/event-stream') # This for Streaming
     return json.dumps(compared_data_stream)
 
 
-
 @app.get("/revise_resume")
 async def revise_resume():
-    print("REVISE") 
-    gptClinet = GptApi()
-
-    with open('./static/resume/extracted_resume.json') as file:
-        extracted_resume_data = file.read()
-
-    with open('./static/resume/job_description.txt') as file:
-        job_description_data = file.read()
-
-    tailored_resume_data_stream = gptClinet.align_resume_info_with_job_description(
-        extracted_resume_data, 
-        job_description_data
-        )
+    tailored_resume_data_stream = await process_revise()
     
     # return StreamingResponse(tailored_resume_data_stream,
     #                            media_type='text/event-stream') # This for Streaming
     return json.dumps(tailored_resume_data_stream)
 
-    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
