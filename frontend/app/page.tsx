75585950a7f42145411390585e44ed54deaccb32
PooriaT <pooria_taghdiri@hotmail.com>
2023-11-22 23:55:55 -0800

    Home page view

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,27 +2,58 @@
 "use client";
 import React, { useState, useEffect, useRef } from 'react';
 import { getFastApiData } from '@/src/utils/fastapiCall';
-import axios, { AxiosResponse }  from 'axios';
+import { AxiosResponse }  from 'axios';
 
 export default function Home() {
-  const [apiData, setApiData] = useState<string | null>(null);
-  const hasEffectRun = useRef(false);
+  const [compareData, setCompareData] = useState<string | null>(null);
+  const [reviseData, setReviseData] = useState<string | null>(null);
+  const [loadingCompare, setLoadingCompare] = useState(false);
+  const [loadingRevise, setLoadingRevise] = useState(false);
+  // const hasEffectRun = useRef(false);
 
-  useEffect(() => {
-    if (!hasEffectRun.current) {
-      const fetchDataFromApi = async () => {
-        try {
-          const response: AxiosResponse = await getFastApiData('compare_resume');
-          setApiData(response.data);
-        } catch (error) {
-          console.error('API Error:', error);
-        }
-      };
-
-      fetchDataFromApi();
-      hasEffectRun.current = true;
+  const handleCompareClick = async () => {
+    try {
+      setLoadingCompare(true);
+      const response: AxiosResponse = await getFastApiData('compare_resume');
+      setCompareData(response.data);
+    } catch (error) {
+      console.error('API Error:', error);
+    } finally {
+      setLoadingCompare(false);
     }
-  }, []);
+  };
+
+  const handleReviseClick = async () => {
+    try {
+      setLoadingRevise(true);
+      const response: AxiosResponse = await getFastApiData('revise_resume');
+      setReviseData(response.data);
+    } catch (error) {
+      console.error('API Error:', error);
+    } finally {
+      setLoadingRevise(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   if (!hasEffectRun.current) {
+  //     const fetchDataFromApi = async () => {
+  //       try {
+  //         const responseCompare: AxiosResponse = await getFastApiData('compare_resume');
+  //         const responseRevise: AxiosResponse = await getFastApiData('revise_resume');
+  //         setCompareData(responseCompare.data);
+  //         setReviseData(responseRevise.data);
+  //       } catch (error) {
+  //         console.error('API Error:', error);
+  //       }
+  //     };
+
+  //     fetchDataFromApi();
+  //     hasEffectRun.current = true;
+  //   }
+  // }, []);
+
+  
 
   return (
     <div className="container mx-auto mt-8 flex-grow">
@@ -33,21 +64,51 @@ export default function Home() {
           Browse
         </button>
       </p>
+
+      <p className='container mx-auto px-4 pb-8 text-red-700 font-serif text-lg font-bold'>
+        CAUTIOUS: Currently due to using the GPT 3.5, this may take a while. BE PATIENT! 😇
+      </p>
+
+      <p className='container mx-auto px-4 pb-6'>
+        What is your request? 
+        <ul>
+          <li className='container mx-auto px-4 pb-6'>
+            Compare your resume with Job Description:  &nbsp;&nbsp;
+            <button
+              className={`bg-red-300 hover:bg-red-700 text-white font-bold py-2 px-4 rounded ${loadingCompare ? 'opacity-50 cursor-not-allowed' : ''}`}
+              onClick={handleCompareClick}
+              disabled={loadingCompare}
+            >
+              {loadingCompare ? 'Comparing...' : 'Compare'}
+            </button>
+          </li>
+          <li className='container mx-auto px-4 pb-6'>
+            Revise your resume based on Job Description:  &nbsp;&nbsp;
+            <button
+              className={`bg-green-300 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ${loadingRevise ? 'opacity-50 cursor-not-allowed' : ''}`}
+              onClick={handleReviseClick}
+              disabled={loadingRevise}
+            >
+              {loadingRevise ? 'Revising...' : 'Revise'}
+            </button>
+          </li>
+        </ul>
+      </p>
       
-      {apiData && (
+      {compareData && (
         <div className='container mx-auto px-4 py-4'>
           <h2 className="px-2 pb-2">Here is the comparison of your resume with Job Description:</h2>
           <div className='box-content px-4 border-4 rounded-lg border-rose-500 border-black bg-gray-400'>
-            <p className='px-4 py-4 font-mono text-black'>{apiData}</p>
+            <p className='px-4 py-4 font-mono text-black'>{compareData}</p>
           </div>
         </div>
       )}
 
-      {apiData && (
+      {reviseData && (
         <div className='container mx-auto px-4 py-4'>
           <h2 className="px-2 pb-2">Here is the your tailored resume based on the Job Description:</h2>
           <div className='box-content px-4 border-4 rounded-lg border-rose-500 border-black bg-gray-400'>
-            <p className='px-4 py-4 font-mono text-black'>{apiData}</p>
+            <p className='px-4 py-4 font-mono text-black'>{reviseData}</p>
           </div>
         </div>
       )}
