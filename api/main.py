2764eda54060ea8f5ead5fc2fc45a18aead1abe4
PooriaT <pooria_taghdiri@hotmail.com>
2023-11-29 17:33:06 -0800

    package.json changes

--- api/main.py
+++ api/main.py
@@ -149,5 +149,5 @@ def revise_resume(client_id: str = Query(...)):
     return json.dumps(tailored_resume_data_stream)
 
 
-if __name__ == "__main__":
-    uvicorn.run('main:app', host="0.0.0.0", port=8000, workers=4)
+# if __name__ == "__main__":
+#     uvicorn.run('main:app', host="0.0.0.0", port=8000, workers=4)
