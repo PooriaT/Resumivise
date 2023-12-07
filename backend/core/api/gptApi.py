395bab95499ca903fc87eedb9da14dd37a0d60cd
PooriaT <pooria_taghdiri@hotmail.com>
2023-12-06 22:56:25 -0800

    Prompt optimization

--- backend/core/api/gptApi.py
+++ backend/core/api/gptApi.py
@@ -67,7 +67,9 @@ class GptApi:
                     "role": "user",
                     "content": f"""I've provided you with all the information from the resume in JSON format, including the job description.
                         I'd like you to carefully tailor the resume based on the job description, covering all sections like summary, 
-                        skills, professional experience, projects, and certifications. Maintain the same format as the original resume data I provided. 
+                        skills, professional experience, projects, and certifications. Change and revise work experience bullet points 
+                        based on the job description.
+                        Maintain the same resume structure as the original one. I mean same structure not the same content. 
                         Here is the updated JSON format of the revised resume:\n\n {extracted_resume_data}. \n\n 
                         You can find the job description here: \n\n {job_description_data} \n\n 
                         The output must be exclusively in JSON format, providing the complete revised resume."""
