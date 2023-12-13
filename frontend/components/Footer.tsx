d12626d9cd6f045c7a1a9f0a13e683cd7fb5839e
Alja Čekada <alja.cekada@gmail.com>
2023-12-13 15:31:16 -0800

    Update Footer.tsx

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,14 +2,18 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-neutral text-base-content">
+    <footer className="footer footer-center p-10 text-base-content rounded bg-neutral">
+      <nav className="grid grid-flow-col gap-4">
+        <a className="link link-hover">Home</a>
+        <a className="link link-hover">About</a>
+        <a className="link link-hover">Support</a>
+      </nav>
       <aside>
         <p>
-          &copy; 2023. Made with 💚 by{" "}
-          <Link href="https://www.linkedin.com/in/pooriataghdiri/">
+          &copy; 2023 Made with 💚 by{" "}
+          <Link className="hover:underline" href="https://www.linkedin.com/in/pooriataghdiri/">
             Pooria Taghdiri
           </Link>
-          .
         </p>
       </aside>
     </footer>
