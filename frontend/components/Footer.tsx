db0c52034c7fc69f6a089cc2bb7b5940fef87f0a
Alja Čekada <alja.cekada@gmail.com>
2023-12-13 15:20:16 -0800

    create footer component

--- frontend/components/Footer.tsx
+++ frontend/components/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+    <footer className="footer footer-center p-4 bg-neutral text-base-content">
       <aside>
         <p>
           &copy; 2023. Made with 💚 by{" "}
