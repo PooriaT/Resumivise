703a974ccbdff1ed96138bcdeb748f0ddcb803c2
Alja Čekada <alja.cekada@gmail.com>
2023-12-13 15:20:26 -0800

    create header component

--- frontend/components/Header.tsx
+++ frontend/components/Header.tsx
@@ -2,14 +2,14 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav className="navbar fixed bg-base-100">
+    <nav className="navbar fixed bg-secondary z-10">
       <div className="flex-1">
         <a className="btn btn-ghost text-xl">Resumivise</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
         <li>
-          <Link href="#home">
+          <Link href="/">
             Home
           </Link>
         </li>
